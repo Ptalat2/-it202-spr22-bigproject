e2cac4f09c05f25ffc35c0c490ed146201430a82
Preet Talati <97310979+Ptalat2@users.noreply.github.com>
2022-04-22 14:40:04 +0000

    caching

--- sw.js
+++ sw.js
@@ -1,17 +1,26 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('sw-cache').then(function(cache) {
-        return cache.add('index.html') && cache.add('API.html') && cache.add('voice.html') && cache.add('userInput.html') 
-        && cache.add('country-codes-lat-long-alpha3.json') 
-        && cache.add('style.css') && cache.add('christmas-tree.png') && cache.add('school.json');
-      })
-    );
-  });
-   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+const cacheName = 'appGet';
+
+self.addEventListener('fetch', (event) => {
+  // Check if this is a request for an image
+  
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the cache first
+      return cache.match(event.request.url).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
+      });
+    }));
+  } 
+  
+);
